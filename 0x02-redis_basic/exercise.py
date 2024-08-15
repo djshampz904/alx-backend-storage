e08c1372021b538308e9f2b1baf10a2d5420deab
djshampz904 <martin.kimani@strathmore.edu>
2024-08-15 14:02:55 +0300

    find all in python

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -26,3 +26,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable = None):
+        """
+        Get a key-value pair from Redis
+        """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """
+        Get a string from Redis
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """
+        Get an int from Redis
+        """
+        return self.get(key, int)
